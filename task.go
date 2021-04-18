b5d00563cdfc20636353bb57d3a17bec8bad15a8
treethought <cpsweene@gmail.com>
2021-04-17 23:06:06 -0700

    style project and contexts

--- task.go
+++ task.go
@@ -1,9 +1,18 @@
 package tudu
 
 import (
+	"strings"
+
 	"github.com/charmbracelet/lipgloss"
 	todo "github.com/treethought/todotxt"
 	tt "github.com/treethought/todotxt"
+)
+
+var (
+	enableStyle    = true
+	completedColor = lipgloss.Color("#3C3C3C") // a dark gray
+	projectColor   = lipgloss.Color("#04B575") // a green
+	contextColor   = lipgloss.Color("#e28743") // a green
 )
 
 type TaskView struct {
@@ -11,19 +20,40 @@ type TaskView struct {
 }
 
 func NewTaskView(task tt.Task) TaskView {
-    return TaskView{task}
+	return TaskView{task}
+
+}
+
+func (t TaskView) styledView() string {
+	style := lipgloss.NewStyle()
+
+	if t.Completed {
+		style = style.Background(completedColor).Strikethrough(true)
+        return style.Render(t.String())
+	} 
+
+
+	s := t.String()
+
+	for _, p := range t.Projects {
+		pstyle := style.Foreground(projectColor)
+		s = strings.ReplaceAll(s, p, pstyle.Render(p))
+	}
+
+	for _, c := range t.Contexts {
+		cstyle := style.Foreground(contextColor)
+		s = strings.ReplaceAll(s, c, cstyle.Render(c))
+	}
+
+	return s
 
 }
 
 func (t TaskView) View() string {
-	completedColor := lipgloss.Color("#3C3C3C") // a dark gray
-	defaultColor := lipgloss.Color("#04B575")   // a green
-
-	if t.Completed {
-		style := lipgloss.NewStyle().Background(completedColor)
-		return style.Render(t.String())
+	if enableStyle {
+		return t.styledView()
 	}
-	style := lipgloss.NewStyle().Background(defaultColor)
-	return style.Render(t.String())
+
+	return t.String()
 
 }
